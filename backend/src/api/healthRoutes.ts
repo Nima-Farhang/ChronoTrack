43153b8e5bb449db79cc3807db66011c59765494
Nima Farhang <nima@farhang.io>
2025-12-11 21:11:50 +0000

    testing CI

--- backend/src/api/healthRoutes.ts
+++ backend/src/api/healthRoutes.ts
@@ -5,7 +5,7 @@ const router = Router();
 
 // GET /health
 router.get("/health", (req: Request, res: Response) => {
-  res.status(200).json({ status: "ok", service: "ChronoTrack is working" });
+  res.status(200).json({ status: "ok", service: "ChronoTrack  is working" });
 });
 
 export default router;
